0110f261b33925cd7858feedc9ad42a284157c28
= <orrjamesw@gmail.com>
2012-07-25 15:03:37 -0400

    Splits Routes into Sitemap, Views, and Routes.

--- src/Routes.hs
+++ src/Routes.hs
@@ -20,28 +20,8 @@ import Web.Routes.Happstack    (implSite)
 import Text.Hamlet
 import Text.Lucius
 
-newtype UserId
-    = UserId { unUserId :: Int }
-      deriving (Eq, Ord, Enum, Read, Show, Data, Typeable, PathInfo)
-
-data Sitemap
-    = Home
-    | Profile UserId
-    | Echo Text
-      deriving (Eq, Ord, Read, Show, Data, Typeable)
-
-$(derivePrinterParsers ''Sitemap)
-
-sitemap :: Router () (Sitemap :- ())
-sitemap =
-    (  rHome
-    <> rProfile . (lit "profile" </> userId)
-    <> rEcho . (lit "message" </> anyText)
-    )
-
-userId :: Router () (UserId :- ())
-userId =
-    xmaph UserId (Just . unUserId) int
+import Views
+import Sitemap (Sitemap(..), unUserId, sitemap)
 
 route :: Sitemap -> RouteT Sitemap (ServerPartT IO) Response
 route url =
@@ -49,35 +29,6 @@ route url =
       Home              -> hamletTest
       (Profile userId)  -> ok $ toResponse $ "Profile" ++ show (unUserId userId)
       (Echo message)    -> ok $ toResponse $ "Message" ++ unpack message
-
-
--- from Tazjin's blog
-convRender :: (url -> [(Text, Maybe Text)] -> Text)
-           -> (url -> [(Text, Text)]-> Text)
-convRender maybeF = 
-  (\url params -> maybeF url $ map (\(t1, t2) -> (t1, Just t2)) params)
-
--- from Tazjin's blog
-renderFunction :: MonadRoute m => m (URL m -> [(Text, Text)] -> Text)
-renderFunction = liftM convRender $ askRouteFn
-
---hamletTest :: UrlHtml Sitemap
---actually apparently it's (MonadRoute m, FilterMonad Response m, URL m ~ Sitemap) => m Response
---so... hmm
--- in future, write all handlers to take acid and renderer, template can then pass it to internal bits?
-hamletTest = do
-                renderer <- renderFunction
-                ok $ toResponse $ [hamlet| 
-                    $doctype 5
-                    <html>
-                      <head>
-                        <link href="/site.css" rel="stylesheet" media="all" type="text/css">
-                      <body>
-                        <div class="container">
-                          <p>"Does it... does it really work???"
-                          <p><a href=@{Home}>Home</a>
-                    |] renderer
-
 
 site :: Site Sitemap (ServerPartT IO Response)
 site =
