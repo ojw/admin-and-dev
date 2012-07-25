160e1b983871de0b4befc52c5b855af29c1564ff
= <orrjamesw@gmail.com>
2012-07-25 13:54:24 -0400

    Gets simple Routes working, removes no longer needed example file from crash course.

--- src/Routes.hs
+++ src/Routes.hs
@@ -1,80 +1,59 @@
-{-# LANGUAGE TemplateHaskell, DeriveDataTypeable, GeneralizedNewtypeDeriving
-           , TypeOperators #-}
+{-# LANGUAGE DeriveDataTypeable, GeneralizedNewtypeDeriving, TemplateHaskell, 
+  TypeOperators, OverloadedStrings #-}
 
 module Routes where
 
-import State (UserId)
-
+import Prelude                 hiding (head, id, (.))
+import Control.Category        (Category(id, (.)))
+import Control.Monad           (msum) 
 import Web.Routes.TH           (derivePathInfo)
-import Data.Data            (Data, Typeable)
-
-import Prelude                 hiding (head)
-
-import Control.Monad           (msum)
 import Data.Data               (Data, Typeable)
-import Data.Monoid             (mconcat)
-import Data.Text               (pack)
-import Happstack.Server        ( Response, ServerPartT, ok, toResponse, simpleHTTP
-                               , nullConf, seeOther, dir, notFound, seeOther)
-import Text.Blaze.Html4.Strict ( (!), html, head, body, title, p, toHtml
-                               , toValue, ol, li, a)
-import Text.Blaze.Html4.Strict.Attributes (href)
+import Data.Text               (Text, unpack)
 import Web.Routes              ( PathInfo(..), RouteT, showURL
                                , runRouteT, Site(..), setDefault, mkSitePI)
-import Web.Routes.TH           (derivePathInfo)
+import Happstack.Server        ( Response, ServerPartT, ok, toResponse, simpleHTTP
+                               , nullConf, seeOther, dir, notFound, seeOther)
+import Web.Routes.Boomerang
+import Text.Boomerang.TH       (derivePrinterParsers)
 import Web.Routes.Happstack    (implSite)
 
+newtype UserId
+    = UserId { unUserId :: Int }
+      deriving (Eq, Ord, Enum, Read, Show, Data, Typeable, PathInfo)
 
 data Sitemap
     = Home
     | Profile UserId
-    | Foo
---    | Adder Int Int
-    deriving (Eq, Ord, Read, Show, Data, Typeable)
+    | Echo Text
+      deriving (Eq, Ord, Read, Show, Data, Typeable)
 
-$(derivePathInfo ''UserId)
-$(derivePathInfo ''Sitemap)
+$(derivePrinterParsers ''Sitemap)
 
--- will later take acid handle
+sitemap :: Router () (Sitemap :- ())
+sitemap =
+    (  rHome
+    <> rProfile . (lit "profile" </> userId)
+    <> rEcho . (lit "message" </> anyText)
+    )
+
+userId :: Router () (UserId :- ())
+userId =
+    xmaph UserId (Just . unUserId) int
+
 route :: Sitemap -> RouteT Sitemap (ServerPartT IO) Response
 route url =
     case url of
-      Home              -> homePage
-      (Profile userId)  -> profilePage userId
-      Foo               -> fooPage
-      --(Adder x1 x2)     -> adderPage x1 x2
-
-homePage :: RouteT Sitemap (ServerPartT IO) Response
-homePage = 
-       ok $ toResponse $
-          html $ do
-            head $ title $ (toHtml "Welcome Home!")
-
-
-profilePage :: UserId -> RouteT Sitemap (ServerPartT IO) Response
-profilePage userId =
-    do homeURL <- showURL Home
-       ok $ toResponse $
-          html $ do
-            head $ title $ (toHtml $ "User " ++ show userId)
-            body $ do
-                   p $ toHtml $ "You are looking at the page of " ++ show userId
-                   p $ do toHtml "Click "
-                          a ! href (toValue homeURL) $ toHtml "here"
-                          toHtml " to return home."
-
-fooPage :: RouteT Sitemap (ServerPartT IO) Response
-fooPage = 
-      ok $ toResponse $ 
-         html $ do head $ title $ (toHtml "FOOOO")
+      Home              -> ok $ toResponse ("Home" :: String)
+      (Profile userId)  -> ok $ toResponse $ "Profile" ++ show (unUserId userId)
+      (Echo message)    -> ok $ toResponse $ "Message" ++ unpack message
 
 site :: Site Sitemap (ServerPartT IO Response)
 site =
-       setDefault Home $ mkSitePI (runRouteT route) 
+       setDefault Home $ boomerangSite (runRouteT route) sitemap
 
 main :: IO ()
 main = simpleHTTP nullConf $
        msum [ dir "favicon.ico" $ notFound (toResponse ())
-            , implSite (pack "http://localhost:8000") (pack "/route") site
-            , seeOther "/route" (toResponse ())
+            , implSite "http://localhost:8000" "/route" site
+            , seeOther ("/route/" :: String) (toResponse ())
             ] 
