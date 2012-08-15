2f3ef0fa9e2c99ef23ad403153ccac5d1661be5a
= <orrjamesw@gmail.com>
2012-08-15 15:00:09 -0400

    Begins trying to integrate server and acid aspects of authentication.  Yuck.

--- src/Authentication.hs
+++ src/Authentication.hs
@@ -8,6 +8,7 @@ where
 
 import Data.Functor      ((<$>))
 import System.FilePath      ((</>))
+import Control.Monad        ( msum, liftM )
 import Control.Monad.State (get, put, gets)
 import Control.Monad.Reader (ask)
 import Data.Maybe (fromMaybe, fromJust)
@@ -15,7 +16,7 @@ import Data.Text (Text)
 import qualified Data.Text as Text
 import Data.SafeCopy        ( SafeCopy, base, deriveSafeCopy )
 import Data.Data            ( Data, Typeable )
-import Data.Lens            ( (%=), (!=), (^$) )
+import Data.Lens            ( (%=), (!=), (^$), (^=) )
 import Data.Lens.Template   ( makeLens )
 import Data.Acid            ( AcidState(..), EventState(..), EventResult(..)
                             , Query(..), QueryEvent(..), Update(..), UpdateEvent(..)
@@ -33,13 +34,19 @@ import Control.Monad.Trans  (MonadIO(..))
 import Data.IxSet           ( Indexable(..), IxSet(..), (@=), Proxy(..), getOne
                             , ixFun, ixSet, updateIx, size, null )
 
-import Prelude  hiding      (null)
+import Prelude  hiding      (null, (.))
 import Data.Map             (Map, insert, adjust, lookup)
 import Data.ByteString      ( ByteString, pack )
 import Crypto.BCrypt        ( validatePassword, hashPasswordUsingPolicy
                             , slowerBcryptHashingPolicy )
 import System.Random        ( StdGen )
 
+import Data.Text.Encoding   ( encodeUtf8 )
+import Control.Category     ( (.) )
+
+import Happstack.Server
+
+import qualified Text.Blaze.Html4.Strict as H
 
 data User = User
     { _userId       :: UserId
@@ -50,10 +57,10 @@ data User = User
     } deriving (Eq, Ord, Typeable)
 
 newtype UserId = UserId Integer deriving (Eq, Ord, Enum, Data, Typeable, SafeCopy)
-newtype SessionId = SessionId Integer deriving (Eq, Ord, Data, Typeable, SafeCopy)
+newtype SessionId = SessionId Integer deriving (Eq, Ord, Enum, Data, Typeable, SafeCopy)
 newtype Email = Email Text deriving (Eq, Ord, Data, Typeable, SafeCopy)
 newtype Name = Name Text deriving (Eq, Ord, Data, Typeable, SafeCopy)
-newtype Password = Password ByteString deriving (Eq, Ord, Data, Typeable, SafeCopy)
+newtype Password = Password { unPassword :: ByteString } deriving (Eq, Ord, Data, Typeable, SafeCopy)
 
 $(makeLens ''User)
 $(deriveSafeCopy 0 'base ''User)
@@ -72,7 +79,6 @@ data AuthenticationState = AuthenticationState
 
 $(makeLens ''AuthenticationState)
 $(deriveSafeCopy 0 'base ''AuthenticationState)
-$(makeAcidic ''AuthenticationState [])
 
 checkEmailAvailability :: Text -> Query AuthenticationState Bool
 checkEmailAvailability newEmail = 
@@ -84,61 +90,70 @@ checkNameAvailability newName =
     do authState <- ask
        return $ null $ (users ^$ authState) @= (Name newName)
 
-addUser :: Email -> Name -> Password -> Update AuthenticationState UserId
-addUser e n p = do  AuthenticationState{..} <- get
-                    users %= updateIx _nextUserId (User _nextUserId e n p Nothing)
-                    nextUserId %= succ
+addUser_ :: Email -> Name -> Password -> Update AuthenticationState UserId
+addUser_ e n p = do  AuthenticationState{..} <- get
+                     users %= updateIx _nextUserId (User _nextUserId e n p Nothing)
+                     nextUserId %= succ
+
+getUser :: Text -> Query AuthenticationState (Maybe User)
+getUser t =
+    do  authState <- ask
+        let byName  = getOne $ (users ^$ authState) @= (Name t)
+            byEmail = getOne $ (users ^$ authState) @= (Email t)
+         in
+            return $ msum [byName, byEmail]
+
+{-
+validateLogin :: Text -> Text -> Query AuthenticationState (Either Text UserId)
+validateLogin nameOrEmail pw =
+    do maybeUser <- getUser nameOrEmail
+       case maybeUser of
+            Nothing     -> return $ Left "Invalid username / email address."
+            Just usr    -> case validatePassword (unPassword (password ^$ usr)) (encodeUtf8 pw) of
+                                True    -> return $ Right (userId ^$ usr)
+                                False   -> return $ Left "Invalid password."
+-}
+
+validateLogin :: User -> Text -> Bool
+validateLogin usr pw = validatePassword (unPassword (password ^$ usr)) (encodeUtf8 pw)
+
+createSession :: UserId -> Update AuthenticationState SessionId
+createSession uid = 
+    do authState <- get
+       let usr = fromJust $ getOne $ (users ^$ authState) @= uid
+           usr' = (sessionId ^= Just (nextSessionId ^$ authState)) usr
+       users %= updateIx uid usr'
+       nextSessionId %= succ
+       
 
 hashPassword :: ByteString -> IO Password
 hashPassword pwd = hashPasswordUsingPolicy slowerBcryptHashingPolicy pwd >>= (return . Password . fromJust)
 
+addUser :: (MonadIO m) => Text -> Text -> Text -> m (Update AuthenticationState UserId)
+addUser e n p =
+    do passwordHash <- liftIO $ hashPassword $ encodeUtf8 p
+       return $ addUser_ (Email e) (Name n) passwordHash 
 
+$(makeAcidic ''AuthenticationState ['getUser])
 
+getCredentials :: RqData (Text, Text)
+getCredentials =
+    do  nameOrEmail <- look "nameOrEmail"
+        password    <- look "password"
+        return (Text.pack nameOrEmail, Text.pack password)
 
+-- horribly wrong, just a test
+tryTest :: AcidState AuthenticationState -> ServerPart String
+tryTest acid =
+    do foo <- look "foo"
+       stuff <- query' acid (GetUser (Text.pack foo))
+       return "ASDF"
 {-
--- internal only
-updatePassword' :: UserId -> Password -> Update AuthenticationState PasswordMap
-updatePassword' uid pwd = passwordMap %= adjust (\p -> pwd) uid
-     
--- internal only
-updateSession' :: UserId -> SessionId -> Update AuthenticationState SessionMap
-updateSession' uid sid = sessionMap %= adjust (\s -> sid) uid
-
--- internal only
-validateLogin' :: UserId -> Password -> PasswordMap -> Bool
-validateLogin' uid pwd pwdMap =
-    case lookup uid pwdMap of
-         Nothing        -> False
-         (Just pwdHash) -> validatePassword pwdHash pwd
-
--- internal only
-addUser' :: UserId -> PasswordHash -> Update AuthenticationState PasswordMap
-addUser' uid pwdh = passwordMap %= insert uid pwdh
-
-addUser :: UserId -> Password -> IO (Update AuthenticationState PasswordMap)
-addUser uid pwd = do pwdh <- hash pwd
-                     return $ addUser' uid pwdh
-
-validateLogin :: UserId -> Password -> Update AuthenticationState (Maybe SessionId)
-validateLogin uid pwd =
-    do (AuthenticationState pm sm _) <- get
-       if validateLogin' uid pwd pm 
-       then (updateSession' uid 1) >> (return $ Just 1)
-       else return Nothing
-
-validateSession :: Maybe UserId -> Maybe SessionId -> Query AuthenticationState (Maybe UserId)
-validateSession _ Nothing = return Nothing
-validateSession Nothing _ = return Nothing
-validateSession (Just uid) (Just sid) = 
-    do (AuthenticationState pm sm _) <- ask
-       case lookup uid sm of
-            Nothing     -> return Nothing
-            (Just sid)  -> return $ Just uid
-
--- suppressing Maybe is clearly a mistake
-hash :: Password -> IO PasswordHash
-hash pwd = hashPasswordUsingPolicy slowerBcryptHashingPolicy pwd >>= (return . fromJust)
-
-$(makeAcidic ''AuthenticationState ['addUser', 'validateSession, 'validateLogin])
-
+tryLogIn :: AcidState AuthenticationState -> ServerPart Response
+tryLogIn acid =
+    do  (nameOrEmail, password) <- getCredentials
+        maybeUser <- query' acid (GetUser nameOrEmail)
+        case maybeUser of
+             Nothing    -> ok $ "FOO"
+             Just usr   -> ok $ show $ validateLogin usr password
 -}
