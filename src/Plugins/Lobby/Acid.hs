f79ed83ff5a8c666cd360ad13ea76b799a1cf969
James Orr <orrjamesw@gmail.com>
2012-09-24 00:44:14 -0400

    Removes some flailing from Lobby.  Prepares to build it gradually.

--- src/Plugins/Lobby/Acid.hs
+++ src/Plugins/Lobby/Acid.hs
@@ -1,4 +1,4 @@
-{-# LANGUAGE DatatypeContexts, ScopedTypeVariables, DeriveDataTypeable, FlexibleContexts #-}
+{-# LANGUAGE DeriveDataTypeable #-}
 -- apparently DatatypeContexts is a misfeature
 -- what should I use instead?
 
@@ -12,7 +12,7 @@ import Data.Data
 
 import Plugins.Auth.Acid        ( UserId )
 import Plugins.Room.Acid.Core   ( RoomId )
-import Plugins.Profile.Acid
+
 import Util.HasAcidState
 
 -- state is type of game state to be stored during gameplay
@@ -22,22 +22,17 @@ import Util.HasAcidState
 --
 -- might need to have class constraint on a parameter! this might be getting tricky...
 --
+-- gonna start by with Lobby as a container with a room...
+-- then add permissions, then maybe options, then games
 
 newtype LobbyId = LobbyId { _unLobbyId :: Int } deriving (Ord, Eq, Data, Typeable, Read, Show)--, SafeCopy)
 
-data (Permissions permissions, Options options) => Lobby state options permissions = Lobby
-    { _users         :: [UserId]
-    , _games         :: IxSet state
-    , _room          :: RoomId
-    , _openGame      :: IxSet options
-    , _permissions   :: permissions
+data Lobby = Lobby
+    { _users        :: [UserId]
+    , _room         :: RoomId
     }
 
--- will go in Game
-class Permissions p where
-    canJoin :: UserId -> ProfileState -> p -> Bool
-
--- also belongs in Game
-class Options o where
--- not sure what goes here yet
-
+data LobbyState = LobbyState
+    { lobbies       :: IxSet Lobby
+    , nextLobbyId   :: LobbyId
+    }
