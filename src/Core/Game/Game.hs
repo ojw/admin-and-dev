5859b08ea9c9aa7a981495148fa04693c1410c7e
= <orrjamesw@gmail.com>
2012-09-25 13:08:45 -0400

    Fiddles in Game.

--- src/Core/Game/Game.hs
+++ src/Core/Game/Game.hs
@@ -33,12 +33,18 @@ class Indexable st => Game st where
 -- Not much we can enforce about html or js.
 -- I would expect the Html / Js to look the same for any st and just handle different states on the client side,
 -- but this allows the game author to decide this (and makes the code legal)
+-- Should look into enforcing more structure here, but it's tricky without enforcing a particular HTML framework.
+-- It's probably correct to just trust that the game author claiming a ByteString is HTML or JS isn't lying.
+-- This is a seriously uninformative class.
 
-class Game st => HasHtmlJsClient st where
+data Html5Game state command = Html5Game
+    { javascript    :: ByteString -- ???
+    , html          :: ByteString
+    , decodeCommand :: ByteString -> Command
+    , encodeState   :: state -> ByteString -- we need different encodings for different users viewing the game
+    }
+
+class Game st => HasHtml5Client st where
     getClientHtml   :: st -> ByteString
     getClientJs     :: st -> ByteString
-
-instance Game Int where
-    runCommand _ _ i = succ i
-    encodeState _ i = L.pack "FOO"
-    newGame _ = 0
+    getClientCss    :: st -> ByteString
