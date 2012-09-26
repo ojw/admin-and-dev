1ffede5774cc82570802dcbe7bab1b11378106a7
= <orrjamesw@gmail.com>
2012-09-26 15:04:43 -0400

    Tweaked comments on Lobby.

--- src/Core/Lobby/Acid.hs
+++ src/Core/Lobby/Acid.hs
@@ -16,15 +16,8 @@ import Core.Room.Acid.Core   ( RoomId )
 
 import Util.HasAcidState
 
--- state is type of game state to be stored during gameplay
--- options is type of all options (including players joining game) to store before game starts
--- permissions is datatype that game will translate to bool for players trying to join lobby
--- possibly have OngoingGame and OpenGame types similarly parameterized in Core.Game for use here
---
--- might need to have class constraint on a parameter! this might be getting tricky...
---
--- gonna start by with Lobby as a container with a room...
--- then add permissions, then maybe options, then games
+-- gonna start with Lobby as a container with a room...
+-- then add permissions, matchmaking, and games
 
 newtype LobbyId = LobbyId { _unLobbyId :: Int } deriving (Ord, Eq, Data, Typeable, Read, Show, SafeCopy)
 
