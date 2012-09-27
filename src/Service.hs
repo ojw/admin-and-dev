79d5ccfd8e1ff296e2d9ea8d4ddabbd04747c7c8
= <orrjamesw@gmail.com>
2012-09-27 14:29:02 -0400

    Adds Service, which might stick around and represent plugins.  Removes Location from Profile.

---
+++ src/Service.hs
@@ -0,0 +1,23 @@
+{-# LANGUAGE Rank2Types #-}
+
+module Service where
+
+import Util.HasAcidState
+import Data.ByteString.Lazy
+import Happstack.Server
+
+-- It can store some stuff, and it can use that stuff to provide a response
+data StatefulService state = StatefulService
+    { initialState  :: state -- what we stick in the Acid type
+    , handler       :: (HasAcidState m state) => ByteString -> m Response -- takes ByteString, probably parses into json, does some stuff and returns a response
+    }
+
+    
+
+-- could have
+-- authService :: StatefulService AuthState
+-- profileService :: StatefulService Profile State
+-- ...
+
+-- acid will be initialized with (initialState authService), etc
+-- handler will call (handler authService), etc
