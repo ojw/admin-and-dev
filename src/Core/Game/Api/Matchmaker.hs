deca9a7dd35c2f27e536585a3080607ca7398ef9
= <orrjamesw@gmail.com>
2012-10-17 11:13:46 -0400

    Refactors giant case statement into little one.

--- src/Core/Game/Api/Matchmaker.hs
+++ src/Core/Game/Api/Matchmaker.hs
@@ -64,34 +64,58 @@ runMatchmakerAPI
     =>  UserId -> AcidState (GameAcid p s o) -> MatchmakerRequest -> m Response
 runMatchmakerAPI userId gameAcid request =
         case request of
-            RequestCreate cap   -> do loc <- query' gameAcid (GetLocation userId)
-                                      case loc of
-                                        Just (InLobby lobbyId) -> do
-                                          roomId <- update' gameAcid CreateRoom
-                                          matchmakerId <- update' gameAcid (CreateMatchmaker userId cap roomId lobbyId)
-                                          update' gameAcid (SetLocation userId (Just (InMatchmaker matchmakerId)))
-                                          ok $ toResponse $ ("Success" :: Text) -- stupid, should return matchmaker data to display
-                                        _   -> ok $ toResponse $ ("You must be in a lobby." :: Text)
-            RequestJoin mId     -> do hasCapacity <- query' gameAcid (MatchmakerHasCapacity mId)
-                                      if hasCapacity 
-                                        then do update' gameAcid (SetLocation userId (Just (InMatchmaker mId)))
-                                                ok $ toResponse $ ("Success" :: Text) -- stupid, should return matchmaker data to display
-                                        else ok $ toResponse $ ("It was full :/" :: Text)
-            RequestLeave        -> do mMatchmakerId <- query' gameAcid (GetLocation userId)
-                                      case mMatchmakerId of
-                                        Nothing                 -> ok $ toResponse $ ("You aren't in that room." :: Text)
-                                        Just (InMatchmaker mId) -> do
-                                            owner <- query' gameAcid (GetMatchmakerOwner mId)
-                                            lobby <- query' gameAcid (GetMatchmakerLobbyId mId)
-                                            if owner == Just userId
-                                                then update' gameAcid (DeleteMatchmaker mId) >> return ()
-                                                else do case lobby of
-                                                            Nothing -> update' gameAcid (SetLocation userId Nothing)
-                                                            Just l  -> update' gameAcid (SetLocation userId (Just (InLobby l)))
-                                                        return ()
-                                            ok $ toResponse $ ("Lobby data here." :: Text)
-            RequestLook         -> do loc <- query' gameAcid (GetLocation userId)
-                                      case loc of
-                                        Just (InLobby lobbyId)  ->  do  matchmakers <- query' gameAcid (LookMatchmakers lobbyId)
-                                                                        ok $ toResponse ("FOO" :: Text) -- $ encode $ matchmakers
-                                        _                       ->  ok $ toResponse ("Not in a lobby." :: Text)
+            RequestCreate cap   -> handleRequestCreate userId gameAcid cap
+            RequestJoin mId     -> handleRequestJoin userId gameAcid mId
+            RequestLeave        -> handleRequestLeave userId gameAcid
+            RequestLook         -> handleRequestLook userId gameAcid
+ 
+handleRequestCreate 
+    ::  (MonadIO m, Happstack m, Typeable p, Typeable s, Typeable o, SafeCopy p, SafeCopy s, SafeCopy o)
+    =>  UserId -> AcidState (GameAcid p s o) -> Int -> m Response
+handleRequestCreate userId gameAcid cap = do
+    loc <- query' gameAcid (GetLocation userId)
+    case loc of
+        Just (InLobby lobbyId)  -> do
+            roomId <- update' gameAcid CreateRoom
+            matchmakerId <- update' gameAcid (CreateMatchmaker userId cap roomId lobbyId)
+            update' gameAcid (SetLocation userId (Just (InMatchmaker matchmakerId)))
+            ok $ toResponse $ ("Success" :: Text) -- stupid, should return matchmaker data to display
+        _                       -> ok $ toResponse $ ("You must be in a lobby." :: Text)
+
+handleRequestJoin
+    ::  (MonadIO m, Happstack m, Typeable p, Typeable s, Typeable o, SafeCopy p, SafeCopy s, SafeCopy o)
+    =>  UserId -> AcidState (GameAcid p s o) -> MatchmakerId -> m Response
+handleRequestJoin userId gameAcid mId = do
+    hasCapacity <- query' gameAcid (MatchmakerHasCapacity mId)
+    if hasCapacity 
+    then do update' gameAcid (SetLocation userId (Just (InMatchmaker mId)))
+            ok $ toResponse $ ("Success" :: Text) -- stupid, should return matchmaker data to display
+    else ok $ toResponse $ ("It was full :/" :: Text)
+
+handleRequestLeave
+    ::  (MonadIO m, Happstack m, Typeable p, Typeable s, Typeable o, SafeCopy p, SafeCopy s, SafeCopy o)
+    =>  UserId -> AcidState (GameAcid p s o) -> m Response
+handleRequestLeave userId gameAcid = do
+    mMatchmakerId <- query' gameAcid (GetLocation userId)
+    case mMatchmakerId of
+        Nothing                 -> ok $ toResponse $ ("You aren't in that room." :: Text)
+        Just (InMatchmaker mId) -> do
+            owner <- query' gameAcid (GetMatchmakerOwner mId)
+            lobby <- query' gameAcid (GetMatchmakerLobbyId mId)
+            if owner == Just userId
+                then update' gameAcid (DeleteMatchmaker mId) >> return ()
+                else do case lobby of
+                            Nothing -> update' gameAcid (SetLocation userId Nothing)
+                            Just l  -> update' gameAcid (SetLocation userId (Just (InLobby l)))
+                        return ()
+            ok $ toResponse $ ("Lobby data here." :: Text)
+
+handleRequestLook
+    ::  (MonadIO m, Happstack m, Typeable p, Typeable s, Typeable o, SafeCopy p, SafeCopy s, SafeCopy o)
+    =>  UserId -> AcidState (GameAcid p s o) -> m Response
+handleRequestLook userId gameAcid = do
+    loc <- query' gameAcid (GetLocation userId)
+    case loc of
+        Just (InLobby lobbyId)  ->  do  matchmakers <- query' gameAcid (LookMatchmakers lobbyId)
+                                        ok $ toResponse ("FOO" :: Text) -- $ encode $ matchmakers
+        _                       ->  ok $ toResponse ("Not in a lobby." :: Text)
