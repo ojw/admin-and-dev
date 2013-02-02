44d120b1c60efdb6172e4c3b8922f79fb2308904
James Orr <orrjamesw@gmail.com>
2013-02-02 10:03:38 -0500

    Adds View class, begins writing instances.

--- src/Framework/Location/Internal/Views/LobbyView.hs
+++ src/Framework/Location/Internal/Views/LobbyView.hs
@@ -1,11 +1,17 @@
-
+{-# LANGUAGE RecordWildCards, MultiParamTypeClasses #-}
 
 module Framework.Location.Internal.Views.LobbyView where
 
+import Data.Maybe                               ( catMaybes )
 import Data.Text                                ( Text )
-import Framework.Profile                        ( UserName )
-import Framework.Location.Internal.Types.Lobby  ( LobbyId )
+import Data.Functor                             ( (<$>) )
+
+import Framework.Auth                           ( UserId )
+import Framework.Profile                        ( UserName, lookupUserName )
+import Framework.Location.Internal.Types.Lobby  ( Lobby(..), LobbyId )
 import Framework.Location.Internal.Types.Chat   ( ChatHolder )
+import Framework.Location.Internal.Types.Location
+import Framework.Location.Internal.Classes.View
 
 data LobbyView = LobbyView
     { name         :: Text
@@ -14,3 +20,15 @@ data LobbyView = LobbyView
     , chats        :: ChatHolder
     , members      :: [UserName]
     } deriving (Ord, Eq, Read, Show) 
+
+instance View Lobby LobbyView where
+    view lobby@Lobby{..} = do
+        memberIds <- getUsers $ InLobby _lobbyId
+        members <- catMaybes <$> mapM lookupUserName memberIds
+        return $ LobbyView
+            { name = _name
+            , description = _description
+            , lobbyId = _lobbyId
+            , chats = _chats
+            , members = members
+            }
