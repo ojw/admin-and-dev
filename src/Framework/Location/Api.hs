7663fff8dc971f7d8726a8ebc1ac753d34b3063f
James Orr <orrjamesw@gmail.com>
2013-02-23 17:28:48 -0500

    Reimplements Chat in Location API.

--- src/Framework/Location/Api.hs
+++ src/Framework/Location/Api.hs
@@ -8,9 +8,10 @@ import Control.Monad.State ( get )
 import Framework.Profile as Profile
 import Framework.Location.Internal.Types.Location
 import Framework.Location.Internal.Instances.Location
-import Framework.Location.Internal.Classes.Location
+import Framework.Location.Internal.Classes.Location hiding ( chat )
 import Framework.Location.Internal.Views.LocationView
 import Framework.Common.Classes as Classes ( view, create, delete', add' )
+import qualified Framework.Location.Internal.Classes.Location as Location
 import Data.Text ( Text, pack )
 
 -- removed UserId since these will run with MonadReader Profile m
@@ -52,6 +53,13 @@ leave = do
     locationId <- getUserLocation userId
     join locationId
 
+chat :: Text -> LocationId -> LocationAction LocationView
+chat text locationId = do
+    userName <- currentUserName
+    Location.chat (userName, text) locationId
+    return $ LVMessage $ pack "Chat added."
+    view locationId
+
 create :: LocationOptions -> LocationAction LocationView
 create locationOptions = do
     add' $ Classes.create locationOptions
@@ -66,7 +74,4 @@ runLocationApi :: LocationApi -> LocationAction LocationView
 runLocationApi (Join locationId) = tryJoin locationId
 runLocationApi Leave = tryLeave
 runLocationApi (Look locationId) = view locationId
-runLocationApi (Chat text locationId) = do
-    userName <- currentUserName
-    chat (userName, text) locationId
-    view locationId
+runLocationApi (Chat text locationId) = chat text locationId
