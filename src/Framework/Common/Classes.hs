afa2a3a5f87fc93c08ff80929ac25ef37ffa1c1c
James Orr <orrjamesw@gmail.com>
2013-02-23 14:40:08 -0500

    Rewrites View with functional dependencies.

--- src/Framework/Common/Classes.hs
+++ src/Framework/Common/Classes.hs
@@ -1,21 +1,13 @@
-{-# LANGUAGE MultiParamTypeClasses #-}
+{-# LANGUAGE MultiParamTypeClasses, FunctionalDependencies #-}
 
 module Framework.Common.Classes where
 
--- many types (+ecosystems) will implement these three classes
--- locations, games, and profiles at least
--- maybe auth?
-
-import Framework.Location.Internal.Types.Location
-
-class MonadFrameworkAction
-
-class View obj view where
+class View obj view m | obj -> m where
     view    :: obj -> m view
 
 class Holder val key holder where
     add     :: val -> holder -> (key, holder)
-    modify  :: key -> (val -> val) -> holder -> holder
+    modify  :: key -> (val -> val) -> holder -> (val, holder)
     delete  :: key -> holder -> holder 
 
 class Create obj options where
