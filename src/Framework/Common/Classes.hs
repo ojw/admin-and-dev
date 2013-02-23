0358348ffa274ee11a10f564603c9ec8fea9fd54
James Orr <orrjamesw@gmail.com>
2013-02-23 15:03:38 -0500

    Fleshes out IndexedContainer typeclass.

--- src/Framework/Common/Classes.hs
+++ src/Framework/Common/Classes.hs
@@ -2,13 +2,34 @@
 
 module Framework.Common.Classes where
 
+import Control.Monad.State ( MonadState, get, put )
+
 class View obj view m | obj -> m where
     view    :: obj -> m view
 
-class Holder val key holder where
-    add     :: val -> holder -> (key, holder)
-    modify  :: key -> (val -> val) -> holder -> (val, holder)
-    delete  :: key -> holder -> holder 
+class IndexedContainer index value container | container -> index value where
+    add     :: value -> container -> (index, container)
+    modify  :: index -> (value -> value) -> container -> (value, container)
+    delete  :: index -> container -> container 
+
+add' :: (IndexedContainer index value container, MonadState container m) => value -> m index
+add' value = do
+    container <- get
+    let (index, container') = add value container
+    put container'
+    return index
+
+modify' :: (IndexedContainer index value container, MonadState container m) => index -> (value -> value) -> m value
+modify' index f = do
+    container <- get
+    let (value, container') = modify index f container
+    put container'
+    return value
+
+delete' :: (IndexedContainer index value container, MonadState container m) => index -> m ()
+delete' index = do
+    container <- get
+    put $ delete index container
 
 class Create obj options where
     new     :: options -> obj 
